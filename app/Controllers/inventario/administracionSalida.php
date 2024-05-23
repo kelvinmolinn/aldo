1a2d4fd140efa4aff697cc6a4e45cfb7e838b7de
kelvinmolinn <47300562+kelvinmolinn@users.noreply.github.com>
2024-05-22 23:38:51 -0600

    fix mostrar salidas

--- app/Controllers/inventario/administracionSalida.php
+++ app/Controllers/inventario/administracionSalida.php
@@ -76,7 +76,7 @@ class AdministracionSalida extends Controller
     
             $data = [
                 'sucursalId'        => $this->request->getPost('sucursalId'),
-                'fhDescargo'        => date('Y-m-d'),
+                'fhDescargo'        => date('Y-m-d H:i:s'),
                 'obsDescargo'       => $this->request->getPost('obsDescargo'),
                 'estadoDescargo'    => "Pendiente"
             ];
@@ -99,10 +99,53 @@ class AdministracionSalida extends Controller
                 // Si el insert falló, devuelve un mensaje de error
                 return $this->response->setJSON([
                     'success' => false,
-                    'mensaje' => 'No se pudo insertar el producto'
+                    'mensaje' => 'No se pudo insertar la salida o descargo'
                 ]);
             }
           
         }
+
+        public function tablaSalida()
+        {
+            $descargosId = $this->request->getPost('descargosId');
+            $mostrarSalida = new inv_descargos();
+            $datos = $mostrarSalida
+            ->select('inv_descargos.descargosId,inv_descargos.fhDescargo,inv_descargos.obsDescargo,conf_sucursales.sucursalId,conf_sucursales.sucursal')
+            ->join('conf_sucursales', 'conf_sucursales.sucursalId = inv_descargos.sucursalId')
+            ->where('inv_descargos.flgElimina', 0)
+            ->findAll();
+        
+            //validar si el producto en la sucursal ya existe sumar la nueva existencia al post
+            // Construye el array de salida  para solo mostrar una 
+            $output['data'] = array();
+            $n = 1; // Variable para contar las filas
+            foreach ($datos as $columna) {
+                // Aquí construye tus columnas
+                $columna1 = $n;
+                $columna2 = "<b>Sucursal:</b> " . $columna['sucursal'];
+                $columna3 = "<b>Fecha/Hora:</b> " . $columna['fhDescargo'];
+                $columna4 = "<b>Motivo/Justifiación:</b> " . $columna['obsDescargo'];
+                $columna5 = " " ;
+    
+                // Agrega la fila al array de salida
+                $output['data'][] = array(
+                    $columna1,
+                    $columna2,
+                    $columna3,
+                    $columna4,
+                    $columna5
+                );
+        
+                $n++;
+            }
+        
+            // Verifica si hay datos
+            if ($n > 1) {
+                return $this->response->setJSON($output);
+            } else {
+                return $this->response->setJSON(array('data' => '')); // No hay datos, devuelve un array vacío
+            }
+        }
+        
     
 }
