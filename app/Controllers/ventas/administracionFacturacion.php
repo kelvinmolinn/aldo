26df7b667f3cfd101a5ecc456c035fc12a67070f
kmolinapal <kmolina@indupal.com>
2024-08-19 15:03:54 -0600

    fix invalidar DTE

--- app/Controllers/ventas/administracionFacturacion.php
+++ app/Controllers/ventas/administracionFacturacion.php
@@ -2768,6 +2768,18 @@ public function invalidarDTE()
     ]);
 }
 
+private function codigoInvalidarGeneracion() {
+    if (function_exists('com_create_guid') === true) {
+        return trim(com_create_guid(), '{}');
+    }
+    
+    $data = PHP_MAJOR_VERSION < 7 ? openssl_random_pseudo_bytes(16) : random_bytes(16);
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);    // Set version to 0100
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);    // Set bits 6-7 to 10
+    
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
+
 
 
     public function imprimirDTE(){
